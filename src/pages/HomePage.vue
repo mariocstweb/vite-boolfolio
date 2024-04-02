9b19cf498db6584c0cd7e75192a63009d5eb86cd
mariocstweb <mariocstmra@gmail.com>
2024-04-02 15:24:15 +0200

    created HomePage and pages folder

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<script>
+import ProjectsList from '../components/ProjectsList.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
+
+export default {
+  name: 'HomePage',
+  components: { ProjectsList },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProject() {
+      axios.get(endpoint).then(res => {
+        this.projects = res.data;
+      })
+
+    }
+
+  },
+  created() {
+    this.fetchProject()
+  }
+
+}
+</script>
+
+<template>
+  <ProjectsList :projects="projects" />
+
+</template>
+
+<style scoped></style>
