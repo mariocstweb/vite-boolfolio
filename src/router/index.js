6a5b0081c0f9eac31984a452dafbf5658cfc9369
mariocstweb <mariocstmra@gmail.com>
2024-04-02 16:12:59 +0200

    added showpage in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router';
  import HomePage from '../pages/HomePage.vue';
+ import ShowPage from '../pages/ShowPage.vue';
 
 
 //  Definisco le rotte
  export default createRouter({
   history: createWebHistory(),
   routes:[
-    {path: '/', component: HomePage}
+    {path: '/', component: HomePage},
+    {path: '/projects/:id', component: ShowPage, name: 'ShowPage'},
+
   ]
  });
